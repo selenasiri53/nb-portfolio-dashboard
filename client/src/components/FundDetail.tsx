bfd1f90bea80c87657b92eef1dc6961591493508
Selena Siri <selenasiri53@gmail.com>
2025-08-29 23:27:15 -0400

    back to funds link for fund detail

--- client/src/components/FundDetail.tsx
+++ client/src/components/FundDetail.tsx
@@ -1,9 +1,24 @@
+import { ArrowLeft } from "lucide-react";
+
 const FundDetail = () => {
   return (
-    <div className="card rounded-2xl p-2 bg-white/15 h-64">
-      Fund detail
-    </div>
-  )
-}
+    <div className=" min-h-screen">
+      <div className="flex gap-4">
+        {/* left */}
+        <h1 className="text-3xl font-thin text-neutral-100">Fund Detail</h1>
+      </div>
 
-export default FundDetail
+      <div>
+        <a href="/funds" className="flex gap-1 items-center text-sm font-thin text-indigo-200 pb-3">
+          <ArrowLeft size={18} />
+          <h1 className="text-sm font-thin">Return to funds</h1>
+        </a>
+        <div className="space-y-3">
+          <div className="lg:col-span-1 card rounded-2xl p-2 lg:p-4 bg-white min-h-screen overflow-scroll"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FundDetail;
--- client/src/pages/Funds.tsx
+++ client/src/pages/Funds.tsx
@@ -3,7 +3,7 @@ import FundsTable from "../components/charts/FundsTable";
 const Funds = () => {
   return (
     <div>
-          <h1 className="text-3xl font-thin text-neutral-100">Funds</h1>
+          <h1 className="text-3xl font-thin text-neutral-100">My Funds</h1>
           <h3 className="text-sm font-thin text-indigo-200 pb-3">
             Select a fund to see more details and stats.
           </h3>
